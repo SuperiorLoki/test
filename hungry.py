6cb654e9acecde813eaaacfdeeb2570c0c89a4dd
Hirithik Pranav <110261542+SuperiorLoki@users.noreply.github.com>
2025-12-24 19:16:00 -0800

    Update hungry.py

--- hungry.py
+++ hungry.py
@@ -1,8 +1,6 @@
 hunger = input("Are you hungry? ")
 if hunger == "yes":
     print("Eat some food")
-    print("eat burgers and fries")
-    print("eat pizzas")
 else:
     print("do your work")
         
