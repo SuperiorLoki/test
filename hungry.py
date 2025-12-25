4afbbae9f1453eea51130e39d0d1c752490f359e
SuperiorLoki <hirithikpranav2006@gmail.com>
2025-12-24 20:10:52 -0800

    added veggies

--- hungry.py
+++ hungry.py
@@ -2,6 +2,7 @@ hunger = input("Are you hungry? ")
 if hunger == "yes":
     print("Eat some food")
     print("eat burgers and fries")
+    print("add some veggies")
 else:
     print("do your work")
         
