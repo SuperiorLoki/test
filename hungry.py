10423227c3fa02aa58970da03559669f9b2824fa
Hirithik Pranav <110261542+SuperiorLoki@users.noreply.github.com>
2025-12-24 20:29:53 -0800

    Update hungry.py
    drinks

--- hungry.py
+++ hungry.py
@@ -4,6 +4,7 @@ if hunger == "yes":
     print("eat burgers and fries")
     print("add some veggies")
     print("and fruits")
+    print("with some drinks")
 else:
     print("do your work")
         
