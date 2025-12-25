3f06aa48c4cd83c8712ac4c0da6f246ca9a80a90
SuperiorLoki <hirithikpranav2006@gmail.com>
2025-12-24 18:28:20 -0800

    fries

--- hungry.py
+++ hungry.py
@@ -3,6 +3,7 @@ if hunger == "yes":
     print("Eat some food")
     print("eat burgers and fries possibly")
     print("maybe pizzas too")
+    print("eat fries")
 else:
     print("Go do some work")
 
