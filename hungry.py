29955168f43a1a5917f8f2355f27e7f815b5fd4b
SuperiorLoki <hirithikpranav2006@gmail.com>
2025-12-24 18:14:53 -0800

    Revert "pizzas"
    This reverts commit 06bf2302166a2bef35fd154eec86be669dd4605f.

--- hungry.py
+++ hungry.py
@@ -1,7 +1,8 @@
 hunger = input("Are you hungry? ")
 if hunger == "yes":
     print("Eat some food")
-    print("Eat pizzas")
+    print("eat burgers and fries possibly")
+    print("maybe pizzas too")
 else:
     print("Go do some work")
 
